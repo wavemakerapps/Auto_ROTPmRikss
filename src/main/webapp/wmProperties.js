624498221bcc57a7248f2b5fb9b50670cf59b565
root <root@7735277998e8>
2017-06-27 01:06:33 +0000

    Initial Push

---
+++ src/main/webapp/wmProperties.js
@@ -0,0 +1,13 @@
+var _WM_APP_PROPERTIES = {
+  "activeTheme" : "material",
+  "dateFormat" : "",
+  "defaultLanguage" : "en",
+  "displayName" : "Auto_ROTPmRikss",
+  "homePage" : "Main",
+  "name" : "Auto_ROTPmRikss",
+  "platformType" : "WEB",
+  "supportedLanguages" : "en",
+  "timeFormat" : "",
+  "type" : "APPLICATION",
+  "version" : "1.0"
+};
